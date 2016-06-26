5e0ca454e1539007647c7392f3a4e5fdbf409608
emptymalei <emptymalei@qq.com>
2016-06-26 12:53:53 -0600

    added gitter badge

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/leima/GitHub/sphinxtr/venv"
+set -gx VIRTUAL_ENV "/Users/leima/GitHub/sphinx-notebook/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
